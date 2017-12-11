043b9ffe49e50bc15a482f98d74cdd6bfadb59c0
Guy Sopher <guyso@wix.com>
2017-12-11 12:48:00 +0200

    add api code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,63 @@
+function renderBlab(blabData) {
+  //create a new blab (maybe clone?)
+  var newBlab = $('#empty-blab').clone();
+
+  //remove the sample blab
+  $(newBlab).attr('id', blabData.id);
+
+  //set the values from the inputs to the new blab
+  $(newBlab).find('.blab-time').html(blabData.createdAt);
+  $(newBlab).find('.blab-email').html(blabData.email);
+  $(newBlab).find('.blab-content').html(blabData.content);
+  $(newBlab).find('.blab-upvotes').html(blabData.upvotes);
+  $(newBlab).find('.blab-image img')
+  .attr('src', 'https://email2pic.herokuapp.com/gravatar/' + blabData.email);
+
+  //make the buttons works
+  $(newBlab).find('.delete-blab').click(function() {
+    $(newBlab).remove();
+    $.ajax({
+      method: 'DELETE',
+      url: '/blabs/' + $(newBlab).attr('id')
+    })
+  });
+  $(newBlab).find('.upvote-blab').click(function() {
+    var currentUpvotes = Number($(newBlab).find('.blab-upvotes').html());
+    currentUpvotes++;
+    $(newBlab).find('.blab-upvotes').html(currentUpvotes);
+    $.ajax({
+      method: 'PUT',
+      url: '/blabs/' + $(newBlab).attr('id'),
+      data: {
+        upvotes: currentUpvotes
+      }
+    })
+  });
+
+  //add the blab to the html
+  $('#empty-blab').before(newBlab);
+}
+
 $(document).ready(function(){
+
+  $.get('/blabs', function(blabs) {
+    for (var i = 0; i < blabs.length; i++) {
+      renderBlab(blabs[i]);
+    }
+  })
+
   $('.blab-send-button').click(function() {
     //read the values in the inputs
     var content = ($('.blab-content-input').val());
     var email = ($('.blab-email-input').val());
 
-    //create a new blab (maybe clone?)
-    var newBlab = $('#empty-blab').clone();
-
-    //remove the sample blab
-    $(newBlab).attr('id', 'new-blab');
-
-    //set the values from the inputs to the new blab
-    $(newBlab).find('.blab-time').html((new Date()).toString());
-    $(newBlab).find('.blab-email').html(email);
-    $(newBlab).find('.blab-content').html(content);
-    $(newBlab).find('.blab-image img')
-    .attr('src', 'https://email2pic.herokuapp.com/gravatar/' + email);
-
-    //make the buttons works
-    $(newBlab).find('.delete-blab').click(function() {
-      $(newBlab).remove();
-    });
-    $(newBlab).find('.upvote-blab').click(function() {
-      var currentUpvotes = Number($(newBlab).find('.blab-upvotes').html());
-      currentUpvotes++;
-      $(newBlab).find('.blab-upvotes').html(currentUpvotes);
-    });
-
-    //add the blab to the html
-    $('#empty-blab').before(newBlab);
+    //send a POST request to the server - save the blab
+    $.post('/blabs', {
+      content: content,
+      email: email,
+      upvotes: 1
+    }, function (savedBlab) {
+      renderBlab(savedBlab);
+    })
   });
 })
--- config/connections.js
+++ config/connections.js
@@ -56,14 +56,11 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   user: 'username', //optional
-  //   password: 'password', //optional
-  //   database: 'your_mongo_db_name_here' //optional
-  // },
+  mongoDB: {
+    adapter: 'sails-mongo',
+    url: 'mongodb://webdev:blabber@ds159274.mlab.com:59274/webdev-wix'
+  },
+
 
   /***************************************************************************
   *                                                                          *
